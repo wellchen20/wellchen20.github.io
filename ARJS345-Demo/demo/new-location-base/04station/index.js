a38c1f34c71a6cccae5b469fc07371abd97f209d
wellchen20 <425369758@qq.com>
2023-05-25 14:03:32 +0800

    Update index.js

--- ARJS345-Demo/demo/new-location-base/04station/index.js
+++ ARJS345-Demo/demo/new-location-base/04station/index.js
@@ -2,11 +2,5 @@ window.onload = () => {
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
         document.getElementById("gpsLab").innerHTML = "gps: " + e.detail.position.latitude + "," + e.detail.position.longitude;
-   
-        document.getElementById("box1").setAttribute('gps-new-entity-place', {
-            latitude: e.detail.position.latitude + 0.001,
-            longitude: e.detail.position.longitude
-        });
-   
     });
 };
